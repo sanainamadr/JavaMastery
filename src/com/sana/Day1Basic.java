ad39c8705f9013b6d77acae524eb2464d6a26eb6
Sana Inamdar <sanainamdar@Sanas-MacBook-Air.local>
2026-03-01 23:33:59 +0530

    Day 1: Java Basics

---
+++ src/com/sana/Day1Basic.java
@@ -0,0 +1,21 @@
+package com.sana;
+
+public class Day1Basic {
+
+	public static void main(String[] args) {
+		int age = 21;
+		double percentage  = 98.33;
+		char grade = 'A';
+		boolean isStudent = true;
+		String name = "Sana";
+		
+		
+		System.out.println("My name : "+ name);
+		System.out.println("My age : "+ age);
+		System.out.println("percentage : "+ percentage);
+		System.out.println("my grade : "+ grade);
+		System.out.println("Is student : "+ isStudent);
+
+	}
+
+}
