be9b9c7a834c29c2a642542a3a91f6b5196bee67
Sana <yourgithubemail@gmail.com>
2026-03-02 12:37:24 +0530

    Day 3: Methods and Overloading

---
+++ src/com/sana/Day3Methods.java
@@ -0,0 +1,22 @@
+package com.sana;
+
+public class Day3Methods {
+	
+	public static int addition(int a, int b) {
+		return a+b;
+	}
+	
+	public static int substraction(int a, int b) {
+		return a-b;
+	}
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println(addition(a,b));
+		System.out.println(substraction(a,b));
+		
+
+	}
+
+}
