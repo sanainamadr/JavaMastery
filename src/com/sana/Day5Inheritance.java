8ed4ec7b6e6902a2ff55fda1c8a10544d4b5d43d
Sana <sana.inamdar005@gmail.com>
2026-03-02 21:54:15 +0530

    Day 5: Inheritance

---
+++ src/com/sana/Day5Inheritance.java
@@ -0,0 +1,35 @@
+package com.sana;
+
+public class Day5Inheritance {
+
+	public static void main(String[] args) {
+		
+		Fish shark = new Fish();
+		shark.eat();
+			
+		
+	}
+
+}
+
+class Animal{
+	String color;
+	
+	void eat() {
+		System.out.print("Eats");
+	}
+	void breath() {
+		System.out.print("Breath");
+	}
+}
+
+class Fish extends Animal{
+	int fin;
+	
+	void swim() {
+		System.out.print("Swimming...");
+	}
+}
+
+
+
